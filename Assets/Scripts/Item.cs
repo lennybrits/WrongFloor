e315959c723aa12c2e4247d97d5bbcb83a25463f
lennybrits <ribeirodebrito@chapman.edu>
2025-11-06 21:54:06 -0800

    added light, textures, and updated item script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 	public GameObject player;
+	public Transform playerDistance; 
     public string itemName = "Item"; 
+	public float minDistance = 6f;
 
 
     private void OnMouseDown()
     {
  		Inventory inventory = player.GetComponent<Inventory>();
-        if (inventory != null)
+		float distance = Vector3.Distance(playerDistance.position, transform.position);
+        if (inventory != null && distance <= minDistance )
         {
             inventory.AddItem(itemName);
             Destroy(gameObject);
